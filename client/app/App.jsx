3189c689319c33f964bd3a14495868b5fa73c5d8
Chris Gray <cmg_business@hotmail.com>
2017-06-02 13:54:16 +0100

    Solve double component rendering, clean up app.jsx closes #28, closes #25

--- client/app/App.jsx
+++ client/app/App.jsx
@@ -17,8 +17,6 @@ import MealList from 'Components/MealList';
 import NewWeek from 'Components/NewWeek';
 
 import 'style-loader!css-loader!sass-loader!./styles/app.scss';
-import 'script-loader!./../node_modules/jquery/dist/jquery.min'
-import 'script-loader!./../node_modules/foundation-sites/dist/js/foundation.min'
 
 $(document).ready(function(){
     $(document).Foundation;
